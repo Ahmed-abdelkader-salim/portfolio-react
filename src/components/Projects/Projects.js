dbf7903cc586bb7c9fa9e7e45c936516c53ea023
Your Name <you@example.com>
2024-10-16 22:21:03 +0300

    Portfolio using react js

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Mastery"
+              description="Mastery Website for students build Bootstrap React js"
+              ghLink="https://github.com/Ahmed-abdelkader-salim/Mastery"
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Bits-0f-C0de"
+              description="Ecommerce website React Reduix reducer axios props"
+              ghLink="https://github.com/Ahmed-abdelkader-salim/Ecommerce_react_redux"
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Editor.io"
+              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              ghLink=""
+              demoLink=""              
+            />
+          </Col>
+
+        
+
+
+        
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
